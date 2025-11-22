2e6780e77085ea13e73a029fb2de1723f3eee175
yimajo <y.imajo@gmail.com>
2025-11-22 20:42:49 +0900

    Update InfinitePagingView+OnSwipeStateChange.swift

--- Sources/InfinitePaging/Modifiers/InfinitePagingView+OnSwipeStateChange.swift
+++ Sources/InfinitePaging/Modifiers/InfinitePagingView+OnSwipeStateChange.swift
@@ -1,12 +1,19 @@
 import SwiftUI
 
-public extension InfinitePagingView {
-    @ViewBuilder
+private struct SwipeStateChangeModifier: ViewModifier {
+    let action: (SwipeState) -> Void
+
+    func body(content: Content) -> some View {
+        content.onPreferenceChange(SwipeStateKey.self) { state in
+            action(state)
+        }
+    }
+}
+
+public extension View {
     func onSwipeStateChange(
         _ action: @escaping (SwipeState) -> Void
     ) -> some View {
-        onPreferenceChange(SwipeStateKey.self) {
-            action($0)
-        }
+        modifier(SwipeStateChangeModifier(action: action))
     }
 }
