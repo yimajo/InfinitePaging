f848ab8998b9067c19c4d8ed7aa6b8ffb4652970
yimajo <y.imajo@gmail.com>
2025-12-09 21:50:10 +0900

    Format

--- Sources/InfinitePaging/PageAlignment.swift
+++ Sources/InfinitePaging/PageAlignment.swift
@@ -15,6 +15,7 @@ public enum PageAlignment: String, CaseIterable {
         switch self {
         case .horizontal:
             size.width
+
         case .vertical:
             size.height
         }
@@ -24,6 +25,7 @@ public enum PageAlignment: String, CaseIterable {
         switch self {
         case .horizontal: 
             CGSize(width: value, height: 0)
+
         case .vertical:
             CGSize(width: 0, height: value)
         }
@@ -34,6 +36,7 @@ public enum PageAlignment: String, CaseIterable {
         switch self {
         case .horizontal:
             translation.height
+
         case .vertical:
             translation.width
         }
